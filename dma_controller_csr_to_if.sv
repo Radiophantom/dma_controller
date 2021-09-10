dd8d8427171600753461f5a339f6fb5707f0b3c3
Sergei Krivchenko <s.krivchenko@metrotek.ru>
2021-09-10 18:07:14 +0300

    Temp commit

--- dma_controller_csr_to_if.sv
+++ dma_controller_csr_to_if.sv
@@ -1,46 +1,81 @@
 module dma_controller_csr_to_if #(
-  parameter DATA_W = 32,
-  parameter A_W = 3
+  parameter CHANNELS_AMOUNT = 4,
+  parameter DATA_W          = 32,
+  parameter ADDR_W          = 8
 )(
-  avalon_mm_if amm_if,
-  dma_conf_if dma_cfg_if [CHANNELS_AMOUNT-1:0]
+  avalon_mm_if  amm_if,
+  dma_conf_if   dma_cfg_if [CHANNELS_AMOUNT-1:0]
 );
 
-logic [DATA_W-1:0] mem [A_W-1:0];
+localparam CHANNEL_REG_AMOUNT = 4*CHANNELS_AMOUNT;
+localparam GENERAL_REG_AMOUNT = 2;
+localparam DATA_B_W           = DATA_W;
+
+logic [DATA_W-1:0] mem_write  [GENERAL_REG_AMOUNT+CHANNEL_REG_AMOUNT-1:0];
+logic [DATA_W-1:0] mem_read   [GENERAL_REG_AMOUNT+CHANNEL_REG_AMOUNT-1:0];
+
+logic              readdatavalid;
+logic [DATA_W-1:0] readdata;
+
+genvar channel_num;
+generate
+
+  for( channel_num = 0; channel_num < CHANNELS_AMOUNT; channel_num++ )
+    begin : gen_dma_channel_cfg
+
+      assign dma_cfg_if.clear_tci_flag    [channel_num] = mem_write[DMA_IFCR_CR][DMA_IFCR_CR_CTCIF+channel_num*4];
+      assign dma_cfg_if.clear_hci_flag    [channel_num] = mem_write[DMA_IFCR_CR][DMA_IFCR_CR_CHTIF+channel_num*4];
+      assign dma_cfg_if.clear_tce_flag    [channel_num] = mem_write[DMA_IFCR_CR][DMA_IFCR_CR_CTEIF+channel_num*4];
+      assign dma_cfg_if.clear_gi_flag     [channel_num] = mem_write[DMA_IFCR_CR][DMA_IFCR_CR_CGIF +channel_num*4];
+
+      assign dma_cfg_if.channel_en        [channel_num] = mem_write[GENERAL_REG_AMOUNT+channel_num*CHANNEL_REG_AMOUNT+DMA_CCR_CR][DMA_CCR_CR_EN];
+      assign dma_cfg_if.full_trans_irq_en [channel_num] = mem_write[GENERAL_REG_AMOUNT+channel_num*CHANNEL_REG_AMOUNT+DMA_CCR_CR][DMA_CCR_CR_TCIE];
+      assign dma_cfg_if.half_trans_irq_en [channel_num] = mem_write[GENERAL_REG_AMOUNT+channel_num*CHANNEL_REG_AMOUNT+DMA_CCR_CR][DMA_CCR_CR_HTIE];
+      assign dma_cfg_if.trans_error_irq_en[channel_num] = mem_write[GENERAL_REG_AMOUNT+channel_num*CHANNEL_REG_AMOUNT+DMA_CCR_CR][DMA_CCR_CR_TEIE];
+      assign dma_cfg_if.trans_direction   [channel_num] = mem_write[GENERAL_REG_AMOUNT+channel_num*CHANNEL_REG_AMOUNT+DMA_CCR_CR][DMA_CCR_CR_DIR ];
+      assign dma_cfg_if.circular_mode_en  [channel_num] = mem_write[GENERAL_REG_AMOUNT+channel_num*CHANNEL_REG_AMOUNT+DMA_CCR_CR][DMA_CCR_CR_CIRC];
+      assign dma_cfg_if.periph_addr_inc_en[channel_num] = mem_write[GENERAL_REG_AMOUNT+channel_num*CHANNEL_REG_AMOUNT+DMA_CCR_CR][DMA_CCR_CR_PINC];
+      assign dma_cfg_if.mem_addr_inc_en   [channel_num] = mem_write[GENERAL_REG_AMOUNT+channel_num*CHANNEL_REG_AMOUNT+DMA_CCR_CR][DMA_CCR_CR_MINC];
+      assign dma_cfg_if.periph_size       [channel_num] = mem_write[GENERAL_REG_AMOUNT+channel_num*CHANNEL_REG_AMOUNT+DMA_CCR_CR][DMA_CCR_CR_PSIZE1:DMA_CCR_CR_PSIZE0];
+      assign dma_cfg_if.mem_size          [channel_num] = mem_write[GENERAL_REG_AMOUNT+channel_num*CHANNEL_REG_AMOUNT+DMA_CCR_CR][DMA_CCR_CR_MSIZE1:DMA_CCR_CR_MSIZE0];
+      assign dma_cfg_if.channel_priority  [channel_num] = mem_write[GENERAL_REG_AMOUNT+channel_num*CHANNEL_REG_AMOUNT+DMA_CCR_CR][DMA_CCR_CR_PL1:DMA_CCR_CR_PL0];
+      assign dma_cfg_if.mem2mem_mode_en   [channel_num] = mem_write[GENERAL_REG_AMOUNT+channel_num*CHANNEL_REG_AMOUNT+DMA_CCR_CR][DMA_CCR_CR_MEM2MEM];
+
+      assign dma_cfg_if.periph_addr       [channel_num] = mem_write[GENERAL_REG_AMOUNT+channel_num*CHANNEL_REG_AMOUNT+DMA_CPAR_CR][DMA_CPAR_CR_PA31:DMA_CPAR_CR_PA0];
+
+      assign dma_cfg_if.mem_addr          [channel_num] = mem_write[GENERAL_REG_AMOUNT+channel_num*CHANNEL_REG_AMOUNT+DMA_CMAR_CR][DMA_CMAR_CR_PA31:DMA_CPAR_CR_PA0];
+
+      assign dma_cfg_if.data_cnt          [channel_num] = amm_if.writedata;
+      assign dma_cfg_if.data_cnt_be       [channel_num] = amm_if.byteenable;
+      assign dma_cfg_if.data_cnt_wr_stb   [channel_num] = amm_if.write && amm_if.address == GENERAL_REG_AMOUNT+channel_num*CHANNEL_REG_AMOUNT+DMA_CNDTR_CR;
+      
+      assign mem_read[DMA_ISR_SR][DMA_ISR_SR_GIF +channel_num*4] = dma_stat_if.set_gi_flag [channel_num];
+      assign mem_read[DMA_ISR_SR][DMA_ISR_SR_TCIF+channel_num*4] = dma_stat_if.set_tci_flag[channel_num];
+      assign mem_read[DMA_ISR_SR][DMA_ISR_SR_HTIF+channel_num*4] = dma_stat_if.set_hti_flag[channel_num];
+      assign mem_read[DMA_ISR_SR][DMA_ISR_SR_TEIF+channel_num*4] = dma_stat_if.set_tei_flag[channel_num];
+
+      assign mem_read[GENERAL_REG_AMOUNT+channel_num*CHANNEL_REG_AMOUNT+DMA_CNDTR_CR]  = dma_stat_if.left_data_cnt[channel_num];
+
+      assign mem_read[GENERAL_REG_AMOUNT+channel_num*CHANNEL_REG_AMOUNT+DMA_CCR_CR]  = mem_read[GENERAL_REG_AMOUNT+channel_num*CHANNEL_REG_AMOUNT+DMA_CCR_CR];
+      assign mem_read[GENERAL_REG_AMOUNT+channel_num*CHANNEL_REG_AMOUNT+DMA_CPAR_CR] = mem_read[GENERAL_REG_AMOUNT+channel_num*CHANNEL_REG_AMOUNT+DMA_CPAR_CR];
+      assign mem_read[GENERAL_REG_AMOUNT+channel_num*CHANNEL_REG_AMOUNT+DMA_CMAR_CR] = mem_read[GENERAL_REG_AMOUNT+channel_num*CHANNEL_REG_AMOUNT+DMA_CMAR_CR];
+    end
+
+endgenerate
 
 always_ff @( posedge amm_if.clk )
   if( amm_if.write )
-    for( int i = 0; i < DATA_B_W; i++ )
-      mem[amm_if.address + i] <= amm_if.wdata[7+i*8 :- 0];
+    for( int byte_num = 0; byte_num < DATA_B_W; byte_num++ )
+      mem_write[amm_if.address + 7 + byte_num*8 -: 8] <= amm_if.writedata[7 + byte_num*8 -: 8];
 
+always_ff @( posedge amm_if.clk )
+  readdatavalid <= amm_if.read;
 
-assign dma_cfg_if.channel_en          = mem[DMA_CCR_CR][DMA_CCR_CR_EN];
-assign dma_cfg_if.full_trans_irq_en   = mem[DMA_CCR_CR][DMA_CCR_CR_TCIE];
-assign dma_cfg_if.half_trans_irq_en   = mem[DMA_CCR_CR][DMA_CCR_CR_HTIE];
-assign dma_cfg_if.trans_error_irq_en  = mem[DMA_CCR_CR][DMA_CCR_CR_TEIE];
-assign dma_cfg_if.trans_direction     = mem[DMA_CCR_CR][DMA_CCR_CR_DIR ];
-assign dma_cfg_if.circular_mode_en    = mem[DMA_CCR_CR][DMA_CCR_CR_CIRC];
-assign dma_cfg_if.periph_addr_inc_en  = mem[DMA_CCR_CR][DMA_CCR_CR_PINC];
-assign dma_cfg_if.mem_addr_inc_en     = mem[DMA_CCR_CR][DMA_CCR_CR_MINC];
-assign dma_cfg_if.periph_size         = mem[DMA_CCR_CR][DMA_CCR_CR_PSIZE1:DMA_CCR_CR_PSIZE0];
-assign dma_cfg_if.mem_size            = mem[DMA_CCR_CR][DMA_CCR_CR_MSIZE1:DMA_CCR_CR_MSIZE0];
-assign dma_cfg_if.channel_priority    = mem[DMA_CCR_CR][DMA_CCR_CR_PL1:DMA_CCR_CR_PL0];
-assign dma_cfg_if.mem2mem_mode_en     = mem[DMA_CCR_CR][DMA_CCR_CR_MEM2MEM];
+always_ff @( posedge amm_if.clk )
+  readdata <= mem_read[amm_if.address+DATA_W-1 -: DATA_W];
 
-assign dma_cfg_if.periph_addr = mem[DMA_CPAR_CR][DMA_CPAR_CR_PA31:DMA_CPAR_CR_PA0];
-assign dma_cfg_if.mem_addr    = mem[DMA_CMAR_CR][DMA_CMAR_CR_PA31:DMA_CPAR_CR_PA0];
-
-assign dma_cfg_if.trans_byte_amount = mem[DMA_CNDTR_CR][DMA_CNDTR_NDT15:DMA_CNDTR_NDT0];
-
-assign dma_cfg_if.clear_tci_flag  = mem[DMA_IFCR_CR][DMA_IFCR_CR_CTCIF];
-assign dma_cfg_if.clear_hci_flag  = mem[DMA_IFCR_CR][DMA_IFCR_CR_CHTIF];
-assign dma_cfg_if.clear_tce_flag  = mem[DMA_IFCR_CR][DMA_IFCR_CR_CTCEF];
-assign dma_cfg_if.clear_gi_flag   = mem[DMA_IFCR_CR][DMA_IFCR_CR_CGIF];
-
-assign mem[DMA_ISR_SR][DMA_ISR_SR_GIF]  = dma_stat_if.set_gi_flag;
-assign mem[DMA_ISR_SR][DMA_ISR_SR_TCIF] = dma_stat_if.set_tci_flag;
-assign mem[DMA_ISR_SR][DMA_ISR_SR_HTIF] = dma_stat_if.set_hti_flag;
-assign mem[DMA_ISR_SR][DMA_ISR_SR_TEIF] = dma_stat_if.set_tei_flag;
+assign amm_if.readdatavalid = readdatavalid;
+assign amm_if.readdata      = readdata;
 
 endmodule : dma_controller_csr_to_if
 
--- dma_controller_engine.sv
+++ dma_controller_engine.sv
@@ -2,23 +2,88 @@ module dma_controller_engine #(
   parameter int CHANNELS_AMOUNT = 1,
   parameter int DATA_W          = 64
 )(
-  input rst_i,
-  input clk_i,
+  input         rst_i,
+  input         clk_i,
 
-  input   request_i     [CHANNELS_AMOUNT-1:0],
-  output  acknowledge_o [CHANNELS_AMOUNT-1:0],
+  input         channel_req_i [CHANNELS_AMOUNT-1:0],
+  output        channel_ack_o [CHANNELS_AMOUNT-1:0],
 
-  avalon_mm_if periph_channel [CHANNELS_AMOUNT-1:0],
-  avalon_mm_if mem_channel    [CHANNELS_AMOUNT-1:0],
+  avalon_mm_if  periph_amm_if [CHANNELS_AMOUNT-1:0],
+  avalon_mm_if  memory_amm_if [CHANNELS_AMOUNT-1:0],
 
-  dma_csr_if csr_if
+  dma_cfg_if    dma_cfg_if    [CHANNELS_AMOUNT-1:0],
+  dma_stat_if   dma_stat_if   [CHANNELS_AMOUNT-1:0]
 );
+
+//***************************************
+// Local parameters
+//***************************************
+
+localparam PERIPH_IND = 1;
+localparam MEMORY_IND = 0;
+localparam DATA_B_W   = DATA_W/8;
+
+//***************************************
+// Functions
+//***************************************
+
+function automatic logic [3:0] inc_step(
+  input logic [1:0] code
+);
+  case( code )
+    2'b00: code = 4'b0001;
+    2'b01: code = 4'b0010;
+    2'b10: code = 4'b0100;
+    2'b11: code = 4'b1000;
+    default: code = 4'b0000;
+  endcase
+endfunction : inc_step
+
+//***************************************
+// Variable declaration
+//***************************************
+
+avalon_mm_if dma_periph_amm_if;
+avalon_mm_if dma_memory_amm_if;
+
+//***********
+// Latch current channel number from arbiter
+//**********
+
+always_ff @( posedge clk_i )
+  if( arb_req_valid && dma_ready )
+    act_channel <= arb_req_channel;
+
+//***********
+// Multiplexing and demultiplexing DMA interface
+//**********
+
+always_comb
+  for( int channel_num = 0; channel_num < CHANNELS_AMOUNT; channel_num++ )
+    if( channel_num == act_channel )
+      periph_amm_if[channel_num] = dma_periph_amm_if;
+    else
+      periph_amm_if[channel_num] = '0;
+
+always_comb
+  for( int channel_num = 0; channel_num < CHANNELS_AMOUNT; channel_num++ )
+    if( channel_num == active_channel )
+      memory_amm_if[channel_num] = dma_memory_amm_if;
+    else
+      memory_amm_if[channel_num] = '0;
+
+//**********
+// Channel request arbiter instance
+//*********
 
 dma_controller_arb #(
 )    (
 
 );
 
+//**********
+// State machine
+//**********
 
 always_ff @( posedge clk_i, posedge rst_i )
   if( rst_i )
@@ -32,7 +97,7 @@ always_comb
     case( state )
       IDLE_S:
         begin
-          if( arb_request_valid )
+          if( arb_req_valid )
             next_state = READ_S;
         end
       READ_S:
@@ -48,20 +113,148 @@ always_comb
             else
               next_state = FINISH_S;
         end
-      FINISH_S:
-        begin
-          next_state = IDLE_S;
-        end
       default: next_state = IDLE_S;
     endcase
   end
 
-always_ff @( posedge clk_i )
-  if( state == IDLE_S && arb_request_valid )
-    active_channel <= arb_request_channel;
+/*
+//******************
+// Internal registers
+//******************
+
+logic [1:0][31:0] allowed_addr_range [5:0];
+
+allowed_addr_range[0][1] = 32'h10_00_00_00;
+allowed_addr_range[0][0] = 32'h00_00_00_00;
+
+allowed_addr_range[1][1] = 32'h20_00_00_00;
+allowed_addr_range[1][0] = 32'h10_00_00_00;
+
+allowed_addr_range[2][1] = 32'h30_00_00_00;
+allowed_addr_range[2][0] = 32'h20_00_00_00;
+
+allowed_addr_range[3][1] = 32'h40_00_00_00;
+allowed_addr_range[3][0] = 32'h30_00_00_00;
+
+allowed_addr_range[4][1] = 32'h50_00_00_00;
+allowed_addr_range[4][0] = 32'h40_00_00_00;
+
+logic [ADDR_RANGE_AMOUNT-1:0] dma_address_allowed_bit;
+logic                         dma_address_allowed;
 
 always_ff @( posedge clk_i )
-  dma_ready <= next_state == IDLE_S;
+  for( int range_num = 0; range_num < ADDR_RANGE_AMOUNT; range_num++ )
+    dma_address_allowed_bit[range_num] <= ( dma_address < allowed_addr_range[range_num][1] ) && ( dma_address > allowed_addr_range[range_num][0] );
+
+always_ff @( posedge clk_i )
+  dma_address_allowed <= &dma_address_allowed_bit;
+*/
+
+always_ff @( posedge clk_i )
+  if( ~dma_cfg_if.channel_en[channel] )
+    periph_addr[channel] <= dma_cfg_if.periph_addr[channel];
+  else
+    if( channel == act_channel && dma_cfg_if.periph_addr_inc_en[channel] && inc_periph_addr_en )
+      periph_addr[channel] <= periph_addr[channel] + inc_step( dma_cfg_if.periph_size[channel];
+
+assign inc_periph_addr_en = ( dma_cfg_if.trans_direction  && state == READ_S  && dma_periph_amm_if.ready && dma_periph_amm_if.read  ) ||
+                            ( ~dma_cfg_if.trans_direction && state == WRITE_S && dma_periph_amm_if.ready && dma_periph_amm_if.write );
+
+always_ff @( posedge clk_i )
+  if( ~dma_cfg_if.channel_en[channel] )
+    half_trans_level <= dma_cfg_if.data_cnt >> 1;
+
+always_ff @( posedge clk_i )
+  half_trans_flag <= ( dma_cfg_if.data_cnt <= half_trans_level );
+
+always_ff @( posedge clk_i )
+  half_trans_flag_d <=  half_trans_flag;
+
+assign half_trans_flag_set_stb = half_trans_flag && ~half_trans_flag_d;
+
+always_ff @( posedge clk_i )
+  if( half_trans_flag_clear_stb )
+    half_trans_flag_bit <= 1'b0;
+  else
+    if( half_trans_flag_set_stb )
+      half_trans_flag_bit <= 1'b1;
+
+assign dma_cfg_if.half_trans_flag[channel] = half_trans_flag_bit;
+
+//****************
+// Periphery side DMA interface 
+//****************
+
+always_ff @( posedge clk_i )
+  if( ~dma_cfg_if.channel_en[act_channel] )
+    first_transaction_was <= 1'b0;
+  else
+    if( dma_cfg_if.trans_direction && state == READ_S && dma_periph_amm_if.ready && dma_periph_amm_if.read )
+      first_transaction_was <= 1'b1;
+    else
+      if( ~dma_cfg_if.trans_direction && state == WRITE_S && dma_periph_amm_if.ready && dma_periph_amm_if.write )
+        first_transaction_was <= 1'b1;
+
+always_ff @( posedge clk_i )
+  if( dma_cfg_if.trans_direction )
+    begin
+      if( state == READ_S && ~first_transaction_was[channel] )
+        dma_periph_amm_if.address <= dma_cfg_if.periph_addr[act_channel];
+      else
+        if( dma_periph_amm_if.read && dma_periph_amm_if.ready && dma_cfg_if.periph_addr_inc_en[act_channel] )
+          dma_periph_amm_if.address <= dma_periph_amm_if.address + inc_step( dma_cfg_if.periph_size );
+    end
+  else
+    begin
+      if( state == WRITE_S )
+        begin
+          dma_periph_amm_if.address   <= dma_cfg_if.periph_addr[act_channel];
+          dma_periph_amm_if.writedata <= temp_data;
+        end
+      else
+        if( dma_periph_amm_if.write && dma_periph_amm_if.ready && dma_cfg_if.periph_addr_inc_en[act_channel] )
+          begin
+            dma_periph_amm_if.address   <= dma_periph_amm_if.address + inc_step( dma_cfg_if.periph_size );
+            dma_periph_amm_if.writedata <= temp_data;
+          end
+    end
+
+always_ff @( posedge clk_i )
+  if( dma_cfg_if.trans_direction )
+    if( state == READ_S )
+      begin
+        dma_periph_amm_if.write <= 1'b0;
+        if( dma_periph_amm_if.ready && dma_periph_amm_if.read )
+          dma_periph_amm_if.read  <= 1'b0;
+        else
+          if( dma_periph_amm_if.ready )
+            dma_periph_amm_if.read  <= 1'b1;
+      end
+    end
+  else
+    if( state == WRITE_S )
+      begin
+        dma_periph_amm_if.read <= 1'b0;
+        if( dma_periph_amm_if.ready && dma_periph_amm_if.read )
+          dma_periph_amm_if.write  <= 1'b0;
+        else
+          if( dma_periph_amm_if.ready )
+            dma_periph_amm_if.write  <= 1'b1;
+      end
+
+//*****************
+// Memory side DMA interface
+//****************
+
+//*******************
+// Arbiter communicate logic
+//*******************
+
+assign dma_ready = next_state == IDLE_S;
+
+//**************************
+// Stat DMA interface
+//**************************
 
 always_ff @( posedge clk_i )
   if( state == IDLE_S && arb_request_valid )
@@ -81,24 +274,32 @@ always_ff @( posedge clk_i )
           2'b10: data_cnt[act_channel] <= data_cnt[act_channel] - 3'd4;
           2'b11: data_cnt[act_channel] <= data_cnt[act_channel] - 4'd8;
 
-assign 
-      if( data_cnt != 0 )
-        if( data_cnt <= [DMA_CCR_CR_MSIZE1:DMA_CCR_CR_MSIZE0] )
-          data_cnt <= '0;
+genvar channel_num;
+generate
+  for( int channel_num = 0; channel_num < CHANNELS_AMOUNT; channel_num++ )
+    begin
 
-always_ff @( posedge clk_i )
-  if( state == START_S )
-    data_cnt[act_channel] <= csr_if.channel_data_cnt[arb_channel_o];
-  else
-    if( amm_if.readdatavalid )
-      case( csr_if.item_addr_mode )
-        2'b00: data_cnt[act_channel] <= data_cnt[act_channel] - 1'd1;
-        2'b01: data_cnt[act_channel] <= data_cnt[act_channel] - 2'd2;
-        2'b10: data_cnt[act_channel] <= data_cnt[act_channel] - 3'd4;
-        2'b11: data_cnt[act_channel] <= data_cnt[act_channel] - 4'd8;
+      assign dec_code_source = ( dma_cfg_if.trans_direction[channel_num] ) ? ( dma_cfg_if.periph_size[channel_num] ):
+                                                                             ( dma_cfg_if.mem_size[channel_num]    );
+      always_comb
+        case( dec_code_source )
+          2'b00: data_cnt_dec_code[channel_num] = 1'd1;
+          2'b01: data_cnt_dec_code[channel_num] = 2'd2;
+          2'b10: data_cnt_dec_code[channel_num] = 3'd4;
+          2'b11: data_cnt_dec_code[channel_num] = 4'd8;
+        endcase
 
-always_ff @( posedge clk_i )
-
+      always_ff @( posedge clk_i )
+        if( ~dma_cfg_if.channel_en[channel_num] && dma_cfg_if.data_cnt_wr_stb[channel_num] )
+          data_cnt[channel_num] <= dma_cfg_if.data_cnt[channel_num];
+        else
+          if( data_cnt_dec[channel_num] )
+            if( data_cnt[channel_num] <= data_cnt_dec_code[channel_num] )
+              data_cnt[channel_num] <= '0;
+            else
+              data_cnt[channel_num] <= data_cnt_dec_code[channel_num];
+    end
+endgenerate
 
 endmodule : dma_controller_engine
 
